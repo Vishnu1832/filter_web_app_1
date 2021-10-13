79a5a8dccdc32f188de732a7000925137af0edc3
Vishnu1832 <84667502+Vishnu1832@users.noreply.github.com>
2021-10-13 12:50:05 +0530

    Add files via upload

---
+++ sunglass.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400,400);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('filter_image');
+    console.log("Image Taken");
+}
